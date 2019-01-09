9558868ff2dbb8f64edf261bdff41252e4b36974
zhouliy <415146419@qq.com>
2019-01-09 22:10:28 +0800

    无

--- src/main/csdnJavaDemos/addDemo.java
+++ src/main/csdnJavaDemos/addDemo.java
@@ -1,3 +1,5 @@
+package csdnJavaDemos;
+
 public class addDemo {
     public static void main(String[] args) {
         int sum = 0;
--- src/main/csdnJavaDemos/arrayDemo.java
+++ src/main/csdnJavaDemos/arrayDemo.java
@@ -2,6 +2,12 @@ package csdnJavaDemos;
 
 public class arrayDemo {
     public static void main(String[] args) {
-        
+        int[] array = {1,2,3,4,5};
+        System.out.println("数组的第一个元素为:" + array[0]);
+        System.out.println("数组为：");
+        for (int i:array
+             ) {
+            System.out.print(i + "\t");
+        }
     }
 }
