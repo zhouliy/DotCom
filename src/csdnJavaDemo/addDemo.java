0ce8648870abfca5ffc04f6681955a5fc5ace244
zhouliy <415146419@qq.com>
2019-01-09 10:37:32 +0800

    新增csdnJavaDemo

---
+++ src/csdnJavaDemo/addDemo.java
@@ -0,0 +1,10 @@
+public class addDemo {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0;i < 101; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
+
